85099b8b6b94507bddfb09d71c2bb6188a5a88fa
v-yichenlu <v-yichenlu@microsoft.com>
2023-10-14 15:43:26 +0800

    feat: merge

--- server/api/latest.ts
+++ server/api/latest.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default defineEventHandler(async (event) => {
-    const fetchUrl = `${process.env.BASE_URL}/latest`;
+    const fetchUrl = `${process.env.BASE_URL}/carousels`;
     const response = await axios.get(fetchUrl);
     const { data } = response;
     return data;
